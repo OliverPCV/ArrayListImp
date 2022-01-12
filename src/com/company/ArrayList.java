62c16488545ed976a5cbfd9cbf037dfd972b9b8f
unknown <oliver.cavallo@educanet.cz>
2022-01-13 00:00:53 +0100

    update

---
+++ src/com/company/ArrayList.java
@@ -0,0 +1,113 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class ArrayList {
+
+    private final int arrLength = 0;
+    private int[] array = new int[arrLength];
+
+    /**
+     *
+     * TODO: returns size of an array
+     */
+    public int size(){
+        return  array.length;
+    }
+
+    /**
+     *
+     * TODO: Add number
+     */
+    public void add(int num) {
+        int[] cloneArr = new int[array.length + 1];
+
+        System.arraycopy(array, 0, cloneArr, 0, array.length);
+        cloneArr[cloneArr.length - 1] = num;
+        array = cloneArr;
+    }
+
+    public void printArray(){
+        System.out.println(Arrays.toString(array));
+    }
+
+    /**
+     *
+     * TODO: Is array empty?
+     */
+    public boolean isEmpty() {
+        return array.length == 0;
+    }
+    /**
+     *
+     * TODO: position of given number
+     */
+    public int indexOf(int num) {
+        int indexOf = 0;
+        for(int i = 0; i < array.length; i++) {
+            if (array[i] == num)
+                indexOf = i;
+        }
+        return indexOf;
+    }
+
+    /**
+     *
+     * TODO: converts the ArrayList into an array
+     */
+    public int[] toArray() {
+        int[] toArray;
+        toArray = array;
+        return toArray;
+    }
+
+
+    /**
+     *
+     * TODO: returns a number from a given position
+     */
+    public int get(int index) {
+        return array[index];
+    }
+
+    /**
+     *
+     * TODO: changes the value of a given index to an input numver
+     */
+    public void set(int index, int num) {
+        array[index] = num;
+    }
+
+    /**
+     *
+     * TODO: Does Array contains input number?
+     */
+    public boolean contains(int num) {
+        for (int j : array)
+            if (j == num)
+                return true;
+        return false;
+    }
+
+
+    /**
+     *
+     * TODO: returns the index of the last number
+     */
+    public int lastIndexOf(int number) {
+        int counter = 0;
+        int[] duplicites = new int[counter];
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == number){
+                counter++;
+                for (int j = 0; j < duplicites.length; j++) {
+                    duplicites[j] = i;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+    ArrayList list = new ArrayList();
+
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(4);
+    list.add(6);
+    list.add(7);
+    list.printArray();
+
+    list.set(1, 5); // změní dvojku na pětku
+
+    System.out.println("Empty question mark: "+ list.isEmpty());
+    System.out.println("IndexOf: " + list.indexOf(3));
+    System.out.println("Number from position 3: " + list.get(3));
+    System.out.println("Velikost údu pole: " + list.size());
+    System.out.println("Contains number 4 question mark: " + list.contains(4));
+    System.out.println("Contains number 12 question mark: " + list.contains(12));
+
+    }
+}
